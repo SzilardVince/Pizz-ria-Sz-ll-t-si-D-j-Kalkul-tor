725d13dc593c9a0af3904b55905a8c6be5e901ac
SzilardVince <szilard.vince.2024d@telekimezotur.hu>
2026-09-23 13:06:01 +0200

    szinek +

--- Pizzéria Szállítási Díj Kalkulátor/Program.cs
+++ Pizzéria Szállítási Díj Kalkulátor/Program.cs
@@ -17,10 +17,16 @@ if (törzse == false)
     }
 }
 double végössz = (rendelésö + szálitás);
-Console.ForegroundColor = ConsoleColor.Green;
+Console.ForegroundColor = ConsoleColor.Magenta;
 Console.WriteLine($"========================================");
+Console.ForegroundColor = ConsoleColor.Red;
 Console.WriteLine($"Vásárló neve:   {vn}");
+Console.ForegroundColor = ConsoleColor.Green;
 Console.WriteLine($"Rendelés összege:   {rendelésö} Ft");
+Console.ForegroundColor = ConsoleColor.DarkYellow;
 Console.WriteLine($"Szállítási díj:   {szálitás} Ft (Törzsvásárlói / 10k feletti ingyenes szállítás)");
+Console.ForegroundColor = ConsoleColor.Cyan;
 Console.WriteLine($"Fizetendő végösszeg:   {végössz} Ft");
+Console.ForegroundColor = ConsoleColor.Yellow;
 Console.WriteLine($"========================================");
+Console.ForegroundColor = ConsoleColor.Black;
