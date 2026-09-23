bc27fa9baca43730861446ec350d858646dde3f6
SzilardVince <szilard.vince.2024d@telekimezotur.hu>
2026-09-23 12:29:11 +0200

    feladat 3

--- Pizzéria Szállítási Díj Kalkulátor/Program.cs
+++ Pizzéria Szállítási Díj Kalkulátor/Program.cs
@@ -1,3 +1,17 @@
 ﻿string vn = "Tóth Bence";
 double rendelésö = 8500;
 bool törzse = true;
+double szálitás = 0;
+if (törzse == false)
+{
+  
+    if (rendelésö >= 5000)
+    {
+        szálitás = 500;
+    }
+    else
+    {
+        szálitás = 1200;
+    }
+}
+double végössz = (rendelésö + szálitás);
